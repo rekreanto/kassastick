72069b56d3aa8b912b2a6c3e85daf95907dc264d
Henning Sato von Rosen <henning.von.rosen@gmail.com>
2022-03-31 14:00:45 +0200

    y

---
+++ kassastick--data.js
@@ -0,0 +1,83 @@
+const data = `
+= 2
+
+/ gips / normal / 13x900x___
+  * 
+    desc Gipsskiva
+    ean  7046638888...
+
+  # 1800
+    ean ...118
+    ccd ooos
+    dim 13x900x1800
+
+  # 2000
+    ean ...132
+    ccd rss
+    dim 13x900x2000
+
+  # 2400
+    ean ...019
+    ccd ryy
+    dim 13x900x2400
+
+  # 2500
+    ean ...033
+    ccd yb
+    dim 13x900x2500
+
+  # 2700
+    ean ...057
+    ccd rb
+    dim 13x900x2700
+
+  # 3000
+    ean ...071
+    ccd yys
+    dim 13x900x3000
+
+
+/ gips / normal / 13x900x___
+    * 
+      desc Gipsskiva
+      ean  5709636009...
+  
+    # 2400
+      ean ...917
+      ccd ooos
+      dim 13x1200x2400
+    
+    # 2500
+      ean ...900
+      ccd ooos
+      dim 13x1200x2500
+
+    # 2700
+      ean ...887
+      ccd ooos
+      dim 13x1200x2700
+
+/ gips / renovering
+  # ytskiva 6mm
+    ean 5709 636 398 240
+    ccd o
+    dim 6x900x2500
+
+  # X-9 utvändig
+    ean 7046 630 121 244
+    ccd ob
+    dim 9x1200x2700
+
+/ gips / kortplank takskiva
+  # 1200mm
+    ean 5709636 408 222
+    ccd vs
+    dim 12,5x600x1200
+  # 2400mm
+    ean 5709636 408 246
+    ccd vvsss
+    dim 12,5x600x2400
+
+
+
+`
