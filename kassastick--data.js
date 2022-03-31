0c72a842e1adc8da48bd02aa36f6c79bf16b4951
Henning Sato von Rosen <henning.von.rosen@gmail.com>
2022-03-31 14:03:50 +0200

    y

--- kassastick--data.js
+++ kassastick--data.js
@@ -6,32 +6,32 @@ const data = `
     desc Gipsskiva
     ean  7046638888...
 
-  # 1800
+  # 1800mm
     ean ...118
     ccd ooos
     dim 13x900x1800
 
-  # 2000
+  # 2000mm
     ean ...132
     ccd rss
     dim 13x900x2000
 
-  # 2400
+  # 2400mm
     ean ...019
     ccd ryy
     dim 13x900x2400
 
-  # 2500
+  # 2500mm
     ean ...033
     ccd yb
     dim 13x900x2500
 
-  # 2700
+  # 2700mm
     ean ...057
     ccd rb
     dim 13x900x2700
 
-  # 3000
+  # 3000mm
     ean ...071
     ccd yys
     dim 13x900x3000
@@ -42,17 +42,17 @@ const data = `
       desc Gipsskiva
       ean  5709636009...
   
-    # 2400
+    # 2400mm
       ean ...917
       ccd ooos
       dim 13x1200x2400
     
-    # 2500
+    # 2500mm
       ean ...900
       ccd ooos
       dim 13x1200x2500
 
-    # 2700
+    # 2700mm
       ean ...887
       ccd ooos
       dim 13x1200x2700
@@ -75,7 +75,7 @@ const data = `
     dim 12,5x600x1200
   # 2400mm
     ean 5709636 408 246
-    ccd vvsss
+    ccd vvss
     dim 12,5x600x2400
 
 
