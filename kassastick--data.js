a99993840b0759e82516e51edb7da1ac91c2d93b
Henning Sato von Rosen <henning.von.rosen@gmail.com>
2022-04-07 14:41:31 +0200

    y

--- kassastick--data.js
+++ kassastick--data.js
@@ -1,6 +1,13 @@
-let source_str = ``;
-
-source_str += `
+const DATA     = {};
+DATA.str       = ``;    //  original input
+DATA.items     = [];    // items yeah
+DATA.groups    = [];  // set in main file
+DATA.txt_ean   = [];    // filter on text
+DATA.ccd_ean   = [];    // filter on color code
+/* DATA.ean2groupnode = {};    // know which group nodes to highlight
+DATA.ean2itemnode  = {};    // know item nodes
+ */
+DATA.str += `
 =2 övrigt
 
 / diverse
@@ -18,7 +25,7 @@ source_str += `
     ean 0200500 15 0735
 
 / prov / kakel
-  * ean 0200500...
+  * ean 0200500 
   # < 30x30cm
     ean ...17 8333
   # > 30x30cm
@@ -27,13 +34,11 @@ source_str += `
     ean ...29 2176
   # mosaik
     ean ...16 8297
-
-/ prov / 
   # tapetbok
     ean 0200500 20 0157 
 
 / dränering
-  * ean 4752053...
+  * ean 4752053
   # slang 10m/rulle
     ean ...01 3815
   # rör 110mm x 4m
@@ -46,14 +51,16 @@ source_str += `
     ean 7331933 59 6111  
   
 / T-list
-  * ean 7393593 00...
+  * ean 7393593 00
   # grå
     ean ...7727
-  # blå ...7710
-  # vit ...7697
+  # blå
+    ean ...7710
+  # vit 
+    ean ...7697
 
 / tomburk plast (inkl lock)
-  * ean 7311230 35...
+  * ean 7311230 35
   # 1L
     ean ...0555
   # 3,4L
@@ -65,94 +72,101 @@ source_str += `
   # hopfällbar korg
     ean 5900791 00 8504
   # barncancerfonden RUNDA UPP
-    ean 0200500 188363
+    ean 0200500 18 8363
 
     `
 
-source_str += `
+DATA.str += `
 = 3 gips
 
 
 / gips / normal / 13x900x___
-  * ean  7046638888...
+  * ean  7046638 88 8
+  * dim  13x900x
 
   # 1800mm
     ean ...118
     ccd ooos
-    dim 13x900x1800
+    dim ...1800
 
   # 2000mm
     ean ...132
     ccd rss
-    dim 13x900x2000
+    dim ...2000
 
   # 2400mm
     ean ...019
     ccd ryy
-    dim 13x900x2400
+    dim 2400
 
   # 2500mm
     ean ...033
-    ccd by
-    dim 13x900x2500
+    ccd yb
+    dim 2500
 
   # 2700mm
     ean ...057
     ccd rb
-    dim 13x900x2700
+    dim 2700
 
   # 3000mm
     ean ...071
     ccd yys
-    dim 13x900x3000
+    dim 3000
 
 
-/ gips / normal / 13x900x___
-    * ean  5709636009...
+/ gips / normal / 13x1200x___
+    * ean  5709636 00 9
+    * dim  13x1200x
     # 2400mm
       ean ...917
       ccd ooos
-      dim 13x1200x2400
+      dim ...2400
     
     # 2500mm
       ean ...900
       ccd ooos
-      dim 13x1200x2500
+      dim ...2500
 
     # 2700mm
       ean ...887
       ccd ooos
-      dim 13x1200x2700
+      dim ...2700
+
+    # 2700mm
+      ean ...887
+      ccd ooos
+      dim ...2700
 
 / gips / renovering
   # ytskiva 6mm
-    ean 5709 636 398 240
+    ean 5709 636 39 8240
     ccd o
     dim 6x900x2500
 
   # X-9 utvändig
-    ean 7046 630 121 244
+    ean 7046 630 12 1244
     ccd ob
     dim 9x1200x2700
 
 / gips / kortplank takskiva
   # 1200mm
-    ean 5709636 408 222
+    ean 5709636 40 8222
     ccd vs
     dim 12,5x600x1200
   # 2400mm
-    ean 5709636 408 246
+    ean 5709636 40 8246
     ccd vvss
     dim 12,5x600x2400
 
 / gips
   # golvgips
-    ean 5709636 398 264
+    ean 5709636 39 8264
     ccd yyys
     dim 13x600x2400
 
   # plan skiva försänkt
-    ean 7332169 017 753
+    ean 7332169 01 7753
     ccd ygg
     dim 13x900x2400
 
